22de0c8de97ddaf6a9c78e169a31ba9611bb725c
Ivan Wilson <ivanwilson@BFranklin.home>
2020-04-17 19:01:05 -0400

    Converting utility.js from jQuery to native JS

--- wp-content/themes/charles-child/build/js/utility.js
+++ wp-content/themes/charles-child/build/js/utility.js
@@ -1,24 +1,50 @@
 // Initialization
-var localPath = "/wp-content/themes/charles-child/";
+const localPath = "/wp-content/themes/charles-child/";
+const html = document.querySelector("html");
+const archives = document.getElementById("archives");
+const inputSearch = document.getElementById("inputSearch");
+const container = document.querySelector(".container");
+const ajaxWindow = document.querySelector(".ajaxWindow");
+const btnNavigation = document.getElementById('btnNavigation');
+const navFind = document.querySelector("[href='#find']");
+//const lastNavItem = document.querySelector(".navPrimary li:last-child");
+const selectMonth = document.querySelector("[name='selectMonthArchive']");
+const search = document.querySelector("[name='search']");
 
-//Navigation
-if (!$("html").hasClass("ie-8")) {
-	$("#btnNavigation, .navPrimary li:nth-child(3) a").on("click", function(){
-		$(".container").toggleClass("jsNavOpen");
-		$(".ajaxWindow").remove();
-		return false;
-	});
+//JS check for navigation placement
+if (html.className.indexOf("no-js") > -1) {
+    html.removeAttribute("class");
 }
 
-//Navigation Form
-$("#archivesForm").on("submit", function(){
-	if ($("#archives").val() == '') { 
-		return false;
-	}		
+//Open navigation in moble/tablet space
+btnNavigation.addEventListener("click", function() {
+    openNavigation();
+    event.preventDefault();
 });
 
-$("#searchForm").on("submit", function(){
-	if ($("#inputSearch").val() == '') { 
-		return false;
-	}	
+//Open navigation in desktop space
+navFind.addEventListener("click", function() {
+    openNavigation();
+    event.preventDefault();
+});
+
+//Open navigation functionality
+function openNavigation() {
+    container.classList.toggle("jsNavOpen");
+    //lastNavItem.parentElementclassList.toggle("selected");	
+    navFind.parentElement.classList.toggle("selected");
+};
+
+//Select Month validation
+selectMonth.addEventListener("click", function() {
+    if (archives.value === "Select Month/Year") {
+        event.preventDefault();
+    }
+});
+
+//Search form validation
+search.addEventListener("click", function() {
+    if (trim(inputSearch.value) === "") {
+        event.preventDefault();
+    }
 });
--- wp-content/themes/charles-child/functions.php
+++ wp-content/themes/charles-child/functions.php
@@ -110,9 +110,11 @@ function html5blank_header_scripts()
 // Load HTML5 Blank conditional scripts
 function html5blank_conditional_scripts()
 {
-    if (is_page('pagenamehere')) {
-        wp_register_script('scriptname', get_template_directory_uri() . '/js/scriptname.js', array('jquery'), '1.0.0'); // Conditional script(s)
-        wp_enqueue_script('scriptname'); // Enqueue it!
+    if (!is_admin()) {
+
+        wp_register_script('utility', get_template_directory_uri() . '/build/js/utility.js', array(), '1.0', true); // Conditional script(s)
+        wp_enqueue_script('utility'); // Enqueue it!
+
     }
 }
 
@@ -354,9 +356,9 @@ function html5blankcomments($comment, $args, $depth)
 
 // Add Actions
 //add_action('init', 'html5blank_header_scripts'); // Add Custom Scripts to wp_head
-//add_action('wp_print_scripts', 'html5blank_conditional_scripts'); // Add Conditional Page Scripts
-add_action('get_header', 'enable_threaded_comments'); // Enable Threaded Comments
-add_action('wp_enqueue_scripts', 'html5blank_styles'); // Add Theme Stylesheet
+add_action( 'wp_enqueue_scripts', 'html5blank_conditional_scripts' ); // Add Conditional Page Scripts
+add_action( 'get_header', 'enable_threaded_comments' ); // Enable Threaded Comments
+add_action( 'wp_enqueue_scripts', 'html5blank_styles' ); // Add Theme Stylesheet
 add_action( 'wp_enqueue_scripts', 'smartwp_remove_wp_block_library_css' ); // Removing Blocks Stylesheet
 add_action('init', 'register_html5_menu'); // Add HTML5 Blank Menu
 add_action('init', 'create_post_type_html5'); // Add our HTML5 Blank Custom Post Type

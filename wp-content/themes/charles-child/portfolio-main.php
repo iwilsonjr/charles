291dc8c60e0e546dad7fc0caa8aa445d272d7bf8
Ivan Wilson <ivanwilson70@gmail.com>
2021-04-02 23:11:16 -0400

    Start of portfolio section

---
+++ wp-content/themes/charles-child/portfolio-main.php
@@ -0,0 +1,58 @@
+<?php
+/*
+Template Name: Main Portfolio
+*/
+?>
+
+<?php get_header(); ?>
+	
+	<!--Content-->
+	<main>
+
+		<div class="content" id="content">	
+	
+		<h1><?php the_title(); ?></h1>
+	
+	<?php if (have_posts()) {
+
+	 while (have_posts()) : the_post(); ?>
+	
+		<!-- article -->
+		<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+		
+			<?php the_content(); ?>
+								
+			<p><?php edit_post_link(); ?></p>
+
+            <div class="blkPortfolio">
+
+                <p>Project List here</p>
+
+            </div>
+			
+		</article>
+		<!-- /article -->
+		
+	<?php endwhile; ?>
+	
+	<?php } else { ?>
+	
+		<!-- article -->
+		<article>
+			<div class="blogEntry"> 
+				<p><?php _e( 'Sorry, nothing to display.', 'html5blank' ); ?></p>
+			</div>
+		</article>
+		<!-- /article -->
+	
+	<?php } ?>
+	
+	
+		</div>
+	
+	</main>
+	<!--Content-->
+	
+<?php get_sidebar(); ?>
+
+<?php get_footer(); ?>
